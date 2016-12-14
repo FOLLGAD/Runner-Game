8a9ed2dfc8d5dfecd854531ec42cdc5bc6c5c450
Emil Ahlbäck <Emil.Ahlback99@gmail.com>
2016-12-14 20:16:35 +0100

    moved function into oject creator

--- js/index.js
+++ js/index.js
@@ -10,74 +10,43 @@ function Update() {
 }
 
 function Tick() {
-  // if right/left & not left/right, move to right/left
-  if (runner.right && !runner.left)
-    runner.velX += runner.runSpeed;
-  else if (runner.left && !runner.right)
-    runner.velX += -runner.runSpeed;
-  if (runner.up && (runner.onGround || Date.now() - runner.jumpTimer < 200)) {
-    runner.velY = -runner.jumpSpeed;
-    if (runner.onGround) runner.jumpTimer = Date.now();
-  } else {
-    runner.jumpTimer = 0;
-  }
-
-  // Checks collision detection ofc
-  CheckCollision();
-
-  runner.x += runner.velX;
-  runner.y += runner.velY;
-
-
-  // accelerate towards ground
-  runner.velY += runner.gravity;
-
-
-  if (runner.y + runner.velY > map.height - runner.height) {
-    runner.y = map.height - runner.height;
-    runner.velY = 0;
-    runner.onGround = true;
-  }
-  // Simulate friction/airdrag
-  runner.velX *= 0.8;
-  runner.velY *= 0.95;
 }
 
-function CheckCollision() {
+function CheckCollision(object) {
   let plength = platforms.length;
   let collision = false;
   // Iterate through platforms
   for (let i = 0; i < plength; i++) {
     // if pos + vel, collide
     if (
-      platforms[i].x < runner.x + runner.width + runner.velX &&
-      platforms[i].x + platforms[i].width > runner.x + runner.velX &&
-      platforms[i].y < runner.y + runner.height &&
-      platforms[i].y + platforms[i].height > runner.y
+      platforms[i].x < object.x + object.width + object.velX &&
+      platforms[i].x + platforms[i].width > object.x + object.velX &&
+      platforms[i].y < object.y + object.height &&
+      platforms[i].y + platforms[i].height > object.y
     ) {
-      if (runner.velX < 0)
-        runner.x = platforms[i].x + platforms[i].width;
+      if (object.velX < 0)
+        object.x = platforms[i].x + platforms[i].width;
       else
-        runner.x = platforms[i].x - runner.width;
-      runner.velX = 0;
+        object.x = platforms[i].x - object.width;
+      object.velX = 0;
     }
     if (
-      platforms[i].x < runner.x + runner.width &&
-      platforms[i].x + platforms[i].width > runner.x &&
-      platforms[i].y < runner.y + runner.height + runner.velY &&
-      platforms[i].y + platforms[i].height > runner.y + runner.velY
+      platforms[i].x < object.x + object.width &&
+      platforms[i].x + platforms[i].width > object.x &&
+      platforms[i].y < object.y + object.height + object.velY &&
+      platforms[i].y + platforms[i].height > object.y + object.velY
     ) {
-      if (runner.velY < 0)
-        runner.y = platforms[i].y + platforms[i].height;
+      if (object.velY < 0)
+        object.y = platforms[i].y + platforms[i].height;
       else
-        runner.y = platforms[i].y - runner.height;
-      if (runner.velY > 0) {
+        object.y = platforms[i].y - object.height;
+      if (object.velY > 0) {
         collision = true;
       }
-      runner.velY = 0;
+      object.velY = 0;
     }
   }
-  runner.onGround = collision;
+  object.onGround = collision;
 }
 
 function UpdateViewport() {
--- js/runner.js
+++ js/runner.js
@@ -18,4 +18,39 @@ function Runner() {
   this.jumpSpeed = 40;
   this.jumpTimer = 0;
   this.gravity = 5;
+
+  // Methods
+  this.tick = () => {
+    // if right/left & not left/right, move to right/left
+    if (this.right && !this.left)
+      this.velX += this.runSpeed;
+    else if (this.left && !this.right)
+      this.velX += -this.runSpeed;
+    if (this.up && (this.onGround || Date.now() - this.jumpTimer < 200)) {
+      this.velY = -this.jumpSpeed;
+      if (this.onGround) this.jumpTimer = Date.now();
+    } else {
+      this.jumpTimer = 0;
+    }
+
+    // Checks collision detection ofc
+    CheckCollision(this);
+
+    this.x += this.velX;
+    this.y += this.velY;
+
+
+    // accelerate towards ground
+    this.velY += this.gravity;
+
+
+    if (this.y + this.velY > map.height - this.height) {
+      this.y = map.height - this.height;
+      this.velY = 0;
+      this.onGround = true;
+    }
+    // Simulate friction/airdrag
+    this.velX *= 0.8;
+    this.velY *= 0.95;
+  }
 }
